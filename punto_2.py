73f5f94e08935f6c63ab73169a9c965854e2fa0b
AlexisU32 <uribea360@gmial.com>
2020-11-01 15:26:10 -0500

    Se termino el segundo punto

---
+++ punto_2.py
@@ -0,0 +1,16 @@
+
+#2) Algoritmo que lea dos números, calculando y escribiendo el valor de su suma, resta, producto y división.
+
+num1 = float(input('Ingrese el primer numero '))
+num2 = float(input('Ingrese el segundo numero '))
+print('\n')
+
+print('La suma de los dos numeros es ' + str(num1 + num2) + '\n')
+print('La resta de los dos numeros es ' + str(num1 - num2) + '\n')
+print('El producto de los dos numeros es ' + str(num2 * num1) + '\n')
+print('La division de los dos numeros es ' + str(num2/num2) + '\n')
+
+
+
+
+
