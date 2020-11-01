e7e800b996a9b3ae61cf54f170f9cc90e0303dcc
AlexisU32 <uribea360@gmial.com>
2020-11-01 18:03:56 -0500

    Se termo de realizar los ultimos puntos del taller dandolo como finalizado

---
+++ punto_12.py
@@ -0,0 +1,8 @@
+# 12) Desarrollar un algoritmo que nos calcule el cuadrado de 
+# los 9 primeros números naturales (recuerda la estructura desde-hasta)
+
+print('\n')
+for i in range(10):
+    print('El cuadrado del ' + str(i+1) + ' es ' + str(pow((i+1),2)))
+print('\n')
+
